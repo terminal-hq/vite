696c10c10205e2638d519d1bdd9eec61cbf1d853
d-liya <dilum14@gmail.com>
2025-04-19 16:01:29 -0230

    added js doc

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+/**
+ * The main component of the application. Currently, it does not render anything.
+ *
+ * @returns JSX element that serves as the root of the application
+ */
 function App() {
   return <></>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+/**
+ * Entry point for the React application.
+ *
+ * This file sets up the React application by:
+ * 1. Importing necessary dependencies and components
+ * 2. Creating a root for the React application in the DOM element with id 'root'
+ * 3. Rendering the App component wrapped in StrictMode
+ *
+ * @module main
+ * @requires react
+ * @requires react-dom/client
+ * @requires ./index.css
+ * @requires ./App.tsx
+ */
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
