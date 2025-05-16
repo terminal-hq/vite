2a2de99ed3da2c5fe46a8e4a5433af1faf725ae9
d-liya <dilum14@gmail.com>
2025-05-15 22:38:55 -0230

    feat: add react-router-dom for routing and wrap App component with BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -14,11 +14,17 @@
  */
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
+import ReactLenis from "lenis/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ReactLenis root>
+        <App />
+      </ReactLenis>
+    </BrowserRouter>
   </StrictMode>
 );
